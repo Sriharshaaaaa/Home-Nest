61daef08e26482a07d3b6fd0c2febe22fe2dc22b
sriharsha <sriharshadabbiru04@gmail.com>
2025-05-20 15:45:17 +0530

    final commit-estate project

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNSOdUg81aDJ3d8HqVQgXJ2jkWESZOS9U",
+  authDomain: "home-nest-cf28c.firebaseapp.com",
+  projectId: "home-nest-cf28c",
+  storageBucket: "home-nest-cf28c.firebasestorage.app",
+  messagingSenderId: "315468539574",
+  appId: "1:315468539574:web:818f2aaf20b437a7156817",
+  measurementId: "G-SKKGB5V2WJ",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
